f61334f2b8ac7d8204c4064ae8781847a0f84d4b
138paulmiller@gmail.com <138paulmiller@gmail.com>
2023-03-16 10:45:59 -0700

    Hotfix

--- source/spriteeditor/spriteeditor.cpp
+++ source/spriteeditor/spriteeditor.cpp
@@ -38,7 +38,7 @@ void SpriteEditor::setup(MainWindow* window)
     sprite_model = new SpriteModel(this);
     sprite_view = ui->sprite_view;
     sprite_view->setModel(sprite_model);
-    spritesheet_view->setMouseHighlightEnabled(false);
+    sprite_view->setMouseHighlightEnabled(false);
 
     spritesheet_model = new SpriteSheetModel(this);
     spritesheet_view = ui->spritesheet_view;
